bb86d2ffaafb385728a5d3ae636c524a58c82051
SimeonAlexandrov <simeon.alexandrov17@gmail.com>
2022-12-01 12:00:52 +0200

    SNR-359 Parse string into more usable vector.

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,5 +1,13 @@
 pub mod solution {
     pub fn output(input: &String) {
-        println!("{input}")
+        let calories_vec: Vec<i32> = input
+            .split("\n")
+            .map(|s| match s.parse::<i32>() {
+                Ok(num) => num,
+                Err(_) => -1,
+            })
+            .collect();
+
+        println!("{:#?}", calories_vec);
     }
 }
