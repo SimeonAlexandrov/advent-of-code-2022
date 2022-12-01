22e3e03daeff4c42372b70a527a4d7584839bd07
SimeonAlexandrov <simeon.alexandrov17@gmail.com>
2022-12-01 12:50:45 +0200

    Implmentation part two.

---
+++ src/day_one/mod.rs
@@ -0,0 +1,70 @@
+pub mod part_one {
+    pub fn output(input: &String) {
+        let calories_vec: Vec<i32> = input
+            .split("\n")
+            .map(|s| match s.parse::<i32>() {
+                Ok(num) => num,
+                Err(_) => -1,
+            })
+            .collect();
+
+        let mut elf_with_most_calories = 0;
+        let mut current_elf = 0;
+        for cal in calories_vec.iter() {
+            if *cal == -1 {
+                // Check if current elf is better than best
+                elf_with_most_calories = update_best_elf(current_elf, elf_with_most_calories);
+                current_elf = 0;
+            } else {
+                current_elf += cal;
+            }
+        }
+        // Check for last sum as well
+        elf_with_most_calories = update_best_elf(current_elf, elf_with_most_calories);
+
+        println!("Result 1: {elf_with_most_calories}")
+    }
+
+    fn update_best_elf(current: i32, best: i32) -> i32 {
+        let new_best = match current > best {
+            true => current,
+            _ => best,
+        };
+        new_best
+    }
+}
+
+pub mod part_two {
+    pub fn output(input: &String) {
+        let calories_vec: Vec<i32> = input
+            .split("\n")
+            .map(|s| match s.parse::<i32>() {
+                Ok(num) => num,
+                Err(_) => -1,
+            })
+            .collect();
+
+        let mut elf_ranking: Vec<i32> = Vec::new();
+
+        let mut current_elf = 0;
+        for cal in calories_vec.iter() {
+            if *cal == -1 {
+                println!("Current elf: {current_elf}");
+                elf_ranking.push(current_elf);
+                current_elf = 0;
+            } else {
+                current_elf += cal;
+            }
+        }
+        println!("Final current elf: {current_elf}");
+        elf_ranking.push(current_elf);
+
+        println!("Ranking: {:#?}", elf_ranking);
+        elf_ranking.sort();
+        elf_ranking.reverse();
+        println!("Sorted ranking: {:#?}", elf_ranking);
+
+        let result = &elf_ranking[0..3];
+        println!("Result {:#?}", result.iter().sum::<i32>());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-mod day1;
+mod day_one;
 mod input;
 
 fn main() {
     let filepath =
-        "/Users/simeon.aleksandrov/Workspace/Learning/aoc-2022/src/day1/input/dummy-input.txt";
+        "/Users/simeon.aleksandrov/Workspace/Learning/aoc-2022/src/day_one/input/dummy-input.txt";
     let data = input::read_file(filepath);
-    day1::solution::output(&data);
+    day_one::part_one::output(&data);
+    day_one::part_two::output(&data);
 }
