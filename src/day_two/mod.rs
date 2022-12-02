663ad366c2c214717c8da7a1f1af001d52db319c
SimeonAlexandrov <simeon.alexandrov17@gmail.com>
2022-12-02 16:23:47 +0200

    initial implementation of part two.

--- src/day_two/mod.rs
+++ src/day_two/mod.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 pub mod part_one {
     #[derive(Debug, Clone)]
     enum Shape {
@@ -83,6 +84,141 @@ pub mod part_one {
         let battles: Vec<Battle> = input.split("\n").map(|s| match_line_to_battle(s)).collect();
         println!("{:#?}", battles);
         let scores: Vec<u8> = battles.iter().map(|b| b.resolve_battle()).collect();
+        println!("Scores: {:#?}", scores);
+        println!(
+            "Result: {}",
+            scores.iter().map(|&score| score as u32).sum::<u32>()
+        )
+    }
+}
+
+pub mod part_two {
+    #[derive(Debug, Clone)]
+    enum Shape {
+        Rock,
+        Paper,
+        Scissors,
+    }
+
+    #[derive(Debug, Clone)]
+    enum ExpectedResult {
+        Win,
+        Draw,
+        Lose,
+    }
+
+    impl ExpectedResult {
+        fn resolve_points(expected_result: &ExpectedResult) -> u8 {
+            match expected_result {
+                ExpectedResult::Win => 6,
+                ExpectedResult::Draw => 3,
+                ExpectedResult::Lose => 0,
+            }
+        }
+    }
+
+    #[derive(Debug)]
+    struct Hand {
+        shape: Shape,
+        points: u8,
+    }
+
+    impl Hand {
+        fn resolve_points(shape: &Shape) -> u8 {
+            match shape {
+                Shape::Rock => 1,     // Rock
+                Shape::Paper => 2,    // Paper
+                Shape::Scissors => 3, // Scissors
+            }
+        }
+    }
+
+    #[derive(Debug)]
+    struct Battle {
+        opponent: Hand,
+        expected_result: ExpectedResult,
+        me: Hand,
+    }
+
+    fn match_char_to_shape(letter: &char) -> Shape {
+        match letter {
+            'A' => Shape::Rock,
+            'B' => Shape::Paper,
+            'C' => Shape::Scissors,
+            _ => panic!("Unexpected shape in input!"),
+        }
+    }
+
+    fn match_char_to_result(letter: &char) -> ExpectedResult {
+        match letter {
+            'X' => ExpectedResult::Lose,
+            'Y' => ExpectedResult::Draw,
+            'Z' => ExpectedResult::Win,
+            _ => panic!("Unexpected result in input!"),
+        }
+    }
+
+    fn match_line_to_battle(line: &str) -> Battle {
+        match line {
+            _ => {
+                let mut lines_with_shapes_and_results = line
+                    .split(' ')
+                    .map(|s| s.chars().next().unwrap_or_default());
+
+                let opponent_shape =
+                    match_char_to_shape(&lines_with_shapes_and_results.next().unwrap());
+                let expected_result =
+                    match_char_to_result(&lines_with_shapes_and_results.next().unwrap());
+
+                return Battle::new(opponent_shape, expected_result);
+            }
+        }
+    }
+
+    impl Battle {
+        fn new(opponent_shape: Shape, expected_result: ExpectedResult) -> Self {
+            let me_shape = Self::resolve_battle(opponent_shape.clone(), expected_result.clone());
+            Self {
+                opponent: Hand {
+                    shape: opponent_shape,
+                    points: 0,
+                },
+                expected_result: expected_result.clone(),
+                me: Hand {
+                    shape: me_shape.clone(),
+                    points: Hand::resolve_points(&me_shape),
+                },
+            }
+        }
+        fn resolve_battle(opponent_shape: Shape, expected_result: ExpectedResult) -> Shape {
+            match (opponent_shape, expected_result) {
+                (Shape::Rock, ExpectedResult::Win) => Shape::Paper,
+                (Shape::Rock, ExpectedResult::Draw) => Shape::Rock,
+                (Shape::Rock, ExpectedResult::Lose) => Shape::Scissors,
+                (Shape::Paper, ExpectedResult::Win) => Shape::Scissors,
+                (Shape::Paper, ExpectedResult::Draw) => Shape::Paper,
+                (Shape::Paper, ExpectedResult::Lose) => Shape::Rock,
+                (Shape::Scissors, ExpectedResult::Win) => Shape::Rock,
+                (Shape::Scissors, ExpectedResult::Draw) => Shape::Scissors,
+                (Shape::Scissors, ExpectedResult::Lose) => Shape::Scissors,
+            }
+        }
+    }
+
+    pub fn output(input: &String) {
+        let battles: Vec<Battle> = input.split("\n").map(|s| match_line_to_battle(s)).collect();
+        println!("{:#?}", battles);
+
+        let scores: Vec<u8> = battles
+            .iter()
+            .map(|b| {
+                ExpectedResult::resolve_points(&b.expected_result)
+                    + Hand::resolve_points(&b.me.shape)
+            })
+            .collect();
+
+        println!("{:#?}", scores);
+
         println!("Scores: {:#?}", scores);
         println!(
             "Result: {}",
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,11 @@ fn main() {
         "/Users/simeon.aleksandrov/Workspace/Learning/aoc-2022/src/day_two/input/dummy-input.txt";
     let _filepath =
         "/Users/simeon.aleksandrov/Workspace/Learning/aoc-2022/src/day_two/input/input.txt";
-    let data = input::read_file(_filepath);
+    let data = input::read_file(_filepath_dummy);
 
     // day_one::part_one::output(&data);
     // day_one::part_two::output(&data);
 
-    day_two::part_one::output(&data);
+    // day_two::part_one::output(&data);
+    day_two::part_two::output(&data);
 }
