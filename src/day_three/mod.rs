9f26ba281c79cbe03f60091c9a01e8e852659edb
SimeonAlexandrov <simeon.alexandrov17@gmail.com>
2022-12-03 19:18:11 +0200

    Initial implementation

--- src/day_three/mod.rs
+++ src/day_three/mod.rs
@@ -50,9 +50,75 @@ pub mod part_one {
 
         println!("{:#?}", backpacks);
 
-        let common_items = backpacks
+        let common_items: Vec<char> = backpacks
             .iter()
-            .map(|b| b.map_backpack_to_common_item().unwrap());
+            .map(|b| b.map_backpack_to_common_item().unwrap())
+            .collect();
+        println!("{:#?}", common_items);
+
+        let priorities: Vec<i32> = common_items.iter().map(|ch| match_priority(*ch)).collect();
+
+        println!("{:#?}", priorities);
+
+        println!("Result: {}", priorities.iter().sum::<i32>())
+    }
+
+    fn match_priority(ch: char) -> i32 {
+        match ch {
+            'a' => 1,
+            'b' => 2,
+            'c' => 3,
+            'd' => 4,
+            'e' => 5,
+            'f' => 6,
+            'g' => 7,
+            'h' => 8,
+            'i' => 9,
+            'j' => 10,
+            'k' => 11,
+            'l' => 12,
+            'm' => 13,
+            'n' => 14,
+            'o' => 15,
+            'p' => 16,
+            'q' => 17,
+            'r' => 18,
+            's' => 19,
+            't' => 20,
+            'u' => 21,
+            'v' => 22,
+            'w' => 23,
+            'x' => 24,
+            'y' => 25,
+            'z' => 26,
+            'A' => 27,
+            'B' => 28,
+            'C' => 29,
+            'D' => 30,
+            'E' => 31,
+            'F' => 32,
+            'G' => 33,
+            'H' => 34,
+            'I' => 35,
+            'J' => 36,
+            'K' => 37,
+            'L' => 38,
+            'M' => 39,
+            'N' => 40,
+            'O' => 41,
+            'P' => 42,
+            'Q' => 43,
+            'R' => 44,
+            'S' => 45,
+            'T' => 46,
+            'U' => 47,
+            'V' => 48,
+            'W' => 49,
+            'X' => 50,
+            'Y' => 51,
+            'Z' => 52,
+            _ => panic!("Unexpected priority"),
+        }
     }
 
     #[cfg(test)]
