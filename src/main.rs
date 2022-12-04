5995057a0c3ca779f5cf7ba64b20c09d5f01d634
SimeonAlexandrov <simeon.alexandrov17@gmail.com>
2022-12-04 13:39:12 +0200

    Prepare for day four.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 mod input;
 // mod day_one;
 // mod day_two;
-mod day_three;
+// mod day_three;
+
+mod day_four;
 
 fn main() {
     let _filepath_dummy =
-        "/Users/simeon.aleksandrov/Workspace/Learning/aoc-2022/src/day_three/input/dummy-input.txt";
+        "/Users/simeon.aleksandrov/Workspace/Learning/aoc-2022/src/day_four/input/dummy-input.txt";
     let _filepath =
-        "/Users/simeon.aleksandrov/Workspace/Learning/aoc-2022/src/day_three/input/input.txt";
-    let data = input::read_file(_filepath);
+        "/Users/simeon.aleksandrov/Workspace/Learning/aoc-2022/src/day_four/input/input.txt";
+    let data = input::read_file(_filepath_dummy);
 
     // day_one::part_one::output(&data);
     // day_one::part_two::output(&data);
@@ -17,5 +19,7 @@ fn main() {
     // day_two::part_two::output(&data);
 
     // day_three::part_one::output(&data);
-    day_three::part_two::output(&data);
+    // day_three::part_two::output(&data);
+
+    day_four::part_one::output(&data);
 }
