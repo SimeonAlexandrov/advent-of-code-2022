a02b4324d4b87b8b22d0471cc818be7da0d1aaf1
SimeonAlexandrov <simeon.alexandrov17@gmail.com>
2022-12-11 20:59:43 +0200

    Start parsing input

--- src/day_11/monkey.rs
+++ src/day_11/monkey.rs
@@ -1,6 +1,7 @@
 pub struct Monkey {
     id: usize,
     starting_items: Vec<usize>,
+    items_inspected_cnt: usize
 }
 
 impl Monkey {
@@ -10,5 +11,13 @@ impl Monkey {
             '*' => old * operand,
             _ => panic!("Unknown operation!"),
         }
+    }
+
+    fn test(&self, item: &usize, operand: &usize) -> {
+        item % operand == 0
+    }
+
+    fn relieve_worry_level(&self, item: &usize) -> usize {
+        item / 3 as usize
     }
 }
--- src/day_11/part_one.rs
+++ src/day_11/part_one.rs
@@ -1,4 +1,22 @@
 pub fn output(input: &String) {
     use super::monkey::Monkey;
     println!("{input}");
+
+    let lines = input.lines();
+
+    let monkeys: Vec<Monkey> = vec![];
+
+    let mut current_monkey: Monkey;
+
+    // for line in lines {
+    //     match line.split(" ") {}
+    // }
+
+    // Monkey's turn:
+    // - inspect
+    // - throw all items left to right
+
+    // Each monkey one turn
+
+    // Item goes to end of list
 }
