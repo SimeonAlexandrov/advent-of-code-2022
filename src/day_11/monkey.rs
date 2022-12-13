fd17610bf2d18f3d670edeb9b8c316070d862138
SimeonAlexandrov <simeon.alexandrov17@gmail.com>
2022-12-13 17:39:44 +0200

    Make it work with Rc<RefCell<Monkey>> maybe?

--- src/day_11/monkey.rs
+++ src/day_11/monkey.rs
@@ -1,46 +1,53 @@
 use std::cell::RefCell;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, Default)]
 pub enum Op {
     Add,
     Multiply,
+    #[default]
+    Noop,
 }
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, Default)]
 pub enum Operand {
     Old,
     Num(usize),
+    #[default]
+    Noop,
 }
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct Monkey {
     pub id: usize,
     pub starting_items: Vec<usize>,
     pub items_inspected_cnt: usize,
-    pub operation: Option<Op>,
-    pub operand: Option<Operand>,
+    pub operation: Op,
+    pub operand: Operand,
     pub divisor: Option<usize>,
     pub true_target: Option<usize>,
     pub false_target: Option<usize>,
 }
 
 impl Monkey {
-    fn operation(&self, old: &usize, op: Op, operand: Operand) -> usize {
-        match op {
-            Op::Add => match operand {
+    pub fn perform_op(&self, old: &usize) -> usize {
+        match self.operation {
+            Op::Add => match self.operand {
                 Operand::Old => old + old,
                 Operand::Num(to_add) => old + to_add,
+                Operand::Noop => panic!("Uninit operand"),
             },
-            Op::Multiply => match operand {
+            Op::Multiply => match self.operand {
                 Operand::Old => old * old,
                 Operand::Num(to_mul) => old * to_mul,
+                Operand::Noop => panic!("Uninit operand"),
             },
+            Op::Noop => panic!("Uninitialized operation"),
         }
     }
 
-    fn test(&self, item: &usize) -> bool {
+    pub fn test(&self, item: usize) -> bool {
         item % self.divisor.unwrap() == 0
     }
 
-    fn relieve_worry_level(&self, item: &usize) -> usize {
+    pub fn relieve_worry_level(&self, item: usize) -> usize {
         item / 3 as usize
     }
 }
--- src/day_11/part_one.rs
+++ src/day_11/part_one.rs
@@ -1,9 +1,4 @@
-use std::{
-    borrow::{Borrow, BorrowMut},
-    cell::RefCell,
-    collections::{HashMap, HashSet},
-    rc::Rc,
-};
+use std::{cell::RefCell, rc::Rc};
 
 use crate::day_11::monkey::{Op, Operand};
 
@@ -13,9 +8,9 @@ pub fn output(input: &String) {
 
     let lines = input.lines();
 
-    let mut monkeys: Vec<Option<Monkey>> = vec![];
+    let mut monkeys: Vec<Rc<RefCell<Monkey>>> = vec![];
 
-    let mut current_monkey: Option<Box<Monkey>> = None;
+    let mut current_monkey: Rc<RefCell<Monkey>> = Rc::new(RefCell::new(Monkey::default()));
 
     for line in lines {
         let words = line.split(" ").collect::<Vec<&str>>();
@@ -25,7 +20,7 @@ pub fn output(input: &String) {
         }
         match words[0] {
             "Monkey" => {
-                let new_monkey = Box::from(Monkey {
+                let new_monkey = Rc::new(RefCell::from(Monkey {
                     id: words[1]
                         .split(":")
                         .collect::<Vec<&str>>()
@@ -35,13 +30,13 @@ pub fn output(input: &String) {
                         .unwrap(),
                     starting_items: vec![],
                     items_inspected_cnt: 0,
-                    operation: None,
-                    operand: None,
+                    operation: Op::Noop,
+                    operand: Operand::Noop,
                     divisor: None,
                     true_target: None,
                     false_target: None,
-                });
-                current_monkey = Some(new_monkey);
+                }));
+                current_monkey = Rc::clone(&new_monkey);
                 // monkeys.push(current_monkey.as_ref().unwrap());
                 println!("New monkey: {:#?}", &current_monkey);
             }
@@ -56,79 +51,81 @@ pub fn output(input: &String) {
                         let without_comma_item = item.replace(",", "");
                         let parsed = without_comma_item.parse::<usize>().unwrap();
 
-                        match current_monkey.as_mut() {
-                            Some(current_monkey) => current_monkey.starting_items.push(parsed),
-                            None => panic!("Current monkey is not set!"),
-                        }
+                        (*current_monkey).borrow_mut().starting_items.push(parsed)
                     }
                     println!("Current monkey: {:#?}", &current_monkey);
                 }
                 "Operation:" => {
-                    match current_monkey.as_mut() {
-                        Some(current_monkey) => match words[6] {
-                            "+" => current_monkey.operation = Some(Op::Add),
-                            "*" => current_monkey.operation = Some(Op::Multiply),
-                            _ => panic!("Unknown operation!"),
-                        },
-                        None => println!("Current monkey is not set!"),
+                    match words[6] {
+                        "+" => (*current_monkey).borrow_mut().operation = Op::Add,
+                        "*" => (*current_monkey).borrow_mut().operation = Op::Multiply,
+                        _ => panic!("Unknown operation!"),
                     };
-                    match current_monkey.as_mut() {
-                        Some(current_monkey) => match words[7] {
-                            "old" => current_monkey.operand = Some(Operand::Old),
-                            other => {
-                                current_monkey.operand =
-                                    Some(Operand::Num(other.parse::<usize>().unwrap()))
-                            }
-                        },
-                        None => println!("Current monkey is not set!"),
+                    match words[7] {
+                        "old" => (*current_monkey).borrow_mut().operand = Operand::Old,
+                        other => {
+                            (*current_monkey).borrow_mut().operand =
+                                Operand::Num(other.parse::<usize>().unwrap())
+                        }
                     };
                     println!("Current monkey: {:#?}", &current_monkey);
                 }
-                "Test:" => match current_monkey.as_mut() {
-                    Some(current_monkey) => match words[5] {
-                        num => current_monkey.divisor = Some(num.parse::<usize>().unwrap()),
-                    },
-                    None => println!("Current monkey is not set!"),
-                },
-                "" => {
-                    match current_monkey.as_mut() {
-                        Some(c_monkey) => match words[5] {
-                            "true:" => {
-                                c_monkey.true_target = Some(words[9].parse::<usize>().unwrap())
-                            }
-                            "false:" => {
-                                c_monkey.false_target = Some(words[9].parse::<usize>().unwrap());
-                                monkeys.push(Some(*c_monkey.clone()))
-                            }
-                            unknown => {
-                                panic!("Unknown word in indented line! {unknown}")
-                            }
-                        },
-                        None => println!("Current monkey is not set!"),
-                    };
-                    println!("Current monkey: {:#?}", current_monkey.borrow_mut());
-                }
-                unknown => {
-                    panic!("Unknown word in indented line! {unknown}")
-                }
+                _ => panic!("Unknown word!"),
             },
+            "Test:" => match words[5] {
+                num => (*current_monkey).borrow_mut().divisor = Some(num.parse::<usize>().unwrap()),
+            },
+            "" => {
+                match words[5] {
+                    "true:" => {
+                        (*current_monkey).borrow_mut().true_target =
+                            Some(words[9].parse::<usize>().unwrap())
+                    }
+                    "false:" => {
+                        (*current_monkey).borrow_mut().false_target =
+                            Some(words[9].parse::<usize>().unwrap());
+                        monkeys.push(Rc::clone(&current_monkey))
+                    }
+                    unknown => {
+                        panic!("Unknown word in indented line! {unknown}")
+                    }
+                };
+                println!("Current monkey: {:#?}", (*current_monkey).borrow());
+            }
             _ => {
                 println!("{:#?}", words);
                 panic!("Unknown word!")
             }
         }
-        // match monkeys.get_mut(current_monkey.as_ref().unwrap().id) {
-        //     Some(monkey) => *monkey = current_monkey.as_ref().unwrap(),
-        //     None => monkeys.push(current_monkey.as_ref().unwrap()),
-        // }
     }
-    println!("Monkeys: {:#?}", &monkeys)
-
-    // Monkey's turn:
-    // - inspect
-    // - throw all items left to right
-
-    // Each monkey one turn
-
-    // Item goes to end of list
+    println!("Monkeys: {:?}", &monkeys);
 }
+
+// for monkey in monkeys {
+//     for item in &monkey.as_ref().unwrap().starting_items {
+//         let mut worry_level = monkey.as_ref().unwrap().perform_op(&item);
+//         println!("New worry level: {}", worry_level);
+
+//         // Relieve stress
+//         worry_level = monkey.as_ref().unwrap().relieve_worry_level(worry_level);
+//         println!("New worry level after relieved stress: {}", worry_level);
+
+//         // Test item
+//         if monkey.as_ref().unwrap().test(worry_level) {
+//             monkeys
+//                 .get_mut(monkey.as_ref().unwrap().true_target.unwrap())
+//                 .unwrap()
+//                 .as_mut()
+//                 .unwrap()
+//                 .starting_items
+//                 .push(worry_level);
+//         }
+//     }
+// }
+// Monkey's turn:
+// - inspect
+// - throw all items left to right
+
+// Each monkey one turn
+
+// Item goes to end of list
